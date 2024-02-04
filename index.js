d84d1ec4cb97914ed06da1f7a65b6e92b49d6ddb
Coding <nehad.jassim@gmail.com>
2024-02-04 22:21:25 +0300

    JS-HW-1a

---
+++ index.js
@@ -0,0 +1,14 @@
+//1
+var name = 'Nehad JM'
+var age = 33
+var isStudent = true
+console.log(name)
+console.log(age)
+console.log(isStudent)
+//2
+var variableToCheck = 2486
+console.log('variableToCheck is a:', typeof variableToCheck)
+//3
+var greeting = 'Hello, '
+var name1 = 'Nehad'
+console.log(greeting + name1)
